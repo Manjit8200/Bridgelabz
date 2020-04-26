411ceac593af686b2c7900c6b70ed53a167d613a
Manjit8200 <44630083+Manjit8200@users.noreply.github.com>
2020-04-26 21:20:24 +0530

    PatternTest

---
+++ Pattern/PatternTest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+echo "Enter word Testing pattern"
+read word
+
+pat=[a-zA-z]{3}
+pat=^[1-9]*
+pat=[1-9a-zA-Z]*$
+
+pat="^[1-9]*[a-zA-Z]{3}[1-9a-zA-Z]*$"
+
+if [[ $word =~ $pat ]];
+then
+	echo yes;
+else
+	echo no;
+fi
